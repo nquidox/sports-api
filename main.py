c791413f751d99cb29ecead497e35bb7faff4f73
nquidox <40396820+nquidox@users.noreply.github.com>
2023-11-13 22:10:20 +0300

    templates removed

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
+from fastapi import FastAPI
 from db_worker import init_db
 import activities
 import authentication
@@ -9,9 +6,6 @@ import users
 
 
 app = FastAPI()
-app.mount('/static', StaticFiles(directory='static'), name='static')
-templates = Jinja2Templates(directory='templates')
-
 app.include_router(authentication.router)
 app.include_router(users.router)
 app.include_router(activities.router)
@@ -20,9 +14,3 @@ app.include_router(activities.router)
 @app.on_event('startup')
 async def run_on_startup():
     init_db()
-
-
-@app.get('/', response_class=HTMLResponse)
-async def get_root(request: Request):
-    return templates.TemplateResponse('index.html',
-                                      {'request': request, 'first': 'Hello', 'second': 'world!'})
