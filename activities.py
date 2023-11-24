272d62d9ab9739701848384c53c3006c4ce9ebed
nquidox <nquidox@gmail.com>
2023-11-24 21:25:49 +0300

    moved from user id to username in the path

--- activities.py
+++ activities.py
@@ -8,12 +8,12 @@ from models import UserModel, ActivityModel
 router = APIRouter(tags=['Activities'])
 
 
-@router.post('/{user_id}/add_activity')
-async def add_activity_run(user_id: int,
-                           current_user: Annotated[UserModel, Depends(get_current_active_user)],
-                           act: ActivityModel):
+@router.post('/{username}/add_activity')
+async def add_activity(username: str,
+                       current_user: Annotated[UserModel, Depends(get_current_active_user)],
+                       act: ActivityModel):
     try:
-        if user_id == current_user['id'] or current_user['is_superuser'] == 1:
+        if username == current_user['username'] or current_user['is_superuser'] == 1:
             sql = ("INSERT INTO activities ("
                    "user_id, title, description, activity_type, laps, distance, date, time_start,"
                    "time_end, published, visibility) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)")
@@ -29,13 +29,13 @@ async def add_activity_run(user_id: int,
         return {'error': e}
 
 
-@router.get('/{user_id}/activities/{act_id}')
-async def get_activity_by_id(user_id: int, act_id: int,
+@router.get('/{username}/activities/{act_id}')
+async def get_activity_by_id(username: str, act_id: int,
                              current_user: Annotated[UserModel, Depends(get_current_active_user)]):
     try:
-        if user_id == current_user['id'] or current_user['is_superuser'] == 1:
+        if username == current_user['username'] or current_user['is_superuser'] == 1:
             sql = "SELECT * FROM activities WHERE user_id = ? AND id = ?"
-            values = (user_id, act_id)
+            values = (current_user['id'], act_id)
             return db_worker('fo', sql, values)
 
         else:
@@ -46,8 +46,8 @@ async def get_activity_by_id(user_id: int, act_id: int,
 
 
 @router.get('/{username}/activities/{act_type}/')
-async def get_activity_by_type(username: str, act_type: str,
-                               current_user: Annotated[UserModel, Depends(get_current_active_user)]):
+async def get_activities_by_type(username: str, act_type: str,
+                                 current_user: Annotated[UserModel, Depends(get_current_active_user)]):
     try:
         if username == current_user['username'] or current_user['is_superuser'] == 1:
             sql = '''SELECT user_id, username, title, description, activity_type,laps, distance, date,
@@ -63,18 +63,18 @@ async def get_activity_by_type(username: str, act_type: str,
         return {'error': e}
 
 
-@router.put('/{user_id}/activities/{act_id}')
-async def edit_activity(user_id: int, act_id: int,
+@router.put('/{username}/activities/{act_id}/')
+async def edit_activity(username: str, act_id: int,
                         act: ActivityModel,
                         current_user: Annotated[UserModel, Depends(get_current_active_user)]):
     try:
-        if user_id == current_user['id'] or current_user['is_superuser'] == 1:
+        if username == current_user['username'] or current_user['is_superuser'] == 1:
             sql = ("UPDATE activities SET title = ?, description = ?, activity_type = ?, laps = ?,"
                    "distance = ?, date = ?, time_start = ?, time_end = ?, published = ?, visibility = ?"
                    "WHERE user_id = ? AND id = ?")
             values = (act.title, act.description, act.activity_type, act.laps, act.distance, act.date,
                       act.time_start, act.time_end, act.published, act.visibility)
-            values += (user_id, act_id)
+            values += (current_user['id'], act_id)
             db_worker('upd', sql, values)
             return act
         else:
@@ -84,13 +84,13 @@ async def edit_activity(user_id: int, act_id: int,
         return {'error': e}
 
 
-@router.delete('/{user_id}/activities/{act_id}')
-async def delete_activity(user_id: int, act_id: int,
+@router.delete('/{username}/activities/{act_id}/')
+async def delete_activity(username: str, act_id: int,
                           current_user: Annotated[UserModel, Depends(get_current_active_user)]):
     try:
-        if user_id == current_user['id'] or current_user['is_superuser'] == 1:
+        if username == current_user['username'] or current_user['is_superuser'] == 1:
             sql = "DELETE FROM activities WHERE user_id = ? AND id = ?"
-            values = (user_id, act_id)
+            values = (current_user['id'], act_id)
             db_worker('del', sql, values)
         else:
             return c403
